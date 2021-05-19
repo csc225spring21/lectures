f3c14231ed8ead23194466d3230b84fdb9e84994
Christopher Siu <christopher@siu.sh>
2021-05-19 11:50:21 -0700

    Added complete pointers example.

--- 05.19_pointers/swap.c
+++ 05.19_pointers/swap.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
 
-void swap(int, int);
+void swap(int *, int *);
 
 int main(void) {
     int z = 1, w = 2;
-    swap(z, w);
+
+    /* To call a function that takes two integer pointers: */
+    swap(&z, &w);
 
     printf("z: %d, w: %d\n", z, w);
 }
 
-void swap(int x, int y) {
-    int temp = x;
-    x = y;
-    y = temp;
+/* To declare a function that takes two integer pointers: */
+void swap(int *x, int *y) {
+    /* To use pointers to modify another functions' locals: */
+    int temp = *x;
+    *x = *y;
+    *y = temp;
 }
