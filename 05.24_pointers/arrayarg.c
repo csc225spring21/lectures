77e18329cf0c81e18ed0fe7a7260146c157e9df2
Christopher Siu <christopher@siu.sh>
2021-05-24 11:11:12 -0700

    Added complete array examples.

--- 05.24_pointers/arrayarg.c
+++ 05.24_pointers/arrayarg.c
@@ -1,10 +1,21 @@
+#include <stdio.h>
+
+/* To declare a function that takes an array as an argument: 
+ * NOTE: "int a2[]" and "int *a2" are equivalent and interchangeable. */
 void foo(int a2[]) {
+    /* To modify an array passed as an argument -- this modifies the caller's
+     * array: */
     a2[0] = 5;
 }
 
 int main(void) {
     int a1[2];
+
+    /* To call a function that takes an array as an argument:
+     * NOTE: An array *is* an address; there is no need for the "&" operator. */
     foo(a1);
+
+    printf("a1[0]: %d\n", a1[0]);
 
     return 0;
 }
--- 05.24_pointers/arrayret.c
+++ 05.24_pointers/arrayret.c
@@ -1,11 +1,22 @@
+#include <stdio.h>
+
 int* foo() {
-    int a2[2];
+    int a2[2] = {94, 327};
+
+    /* NOTE: This returns a pointer to a local variable that is about to be
+     *       deallocated -- we should *never* do this. */
     return a2;
 }
 
 int main(void) {
     int *a1;
+
+    /* NOTE: This points "a1" at a memory location that is going to be
+     *       overwritten as soon as we call another function. */
     a1 = foo();
+
+    printf("a1[0]: %d\n", a1[0]);
+    printf("a1[0]: %d\n", a1[0]);
 
     return 0;
 }
