f6e04dcbd91d20cb06ef6525627e545c5e087c13
Christopher Siu <christopher@siu.sh>
2021-06-02 11:00:53 -0700

    Added complete structure examples.

--- 06.02_structures/point.c
+++ 06.02_structures/point.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+#include "point.h"
 
 int main(void) {
-    int x = 0, y = 0;
+    /* To declare and initialize a variable of type "struct point": */
+    struct point pt = {0, 0};
 
-    x++;
-    y = x + 1;
+    /* To mutate the members of a structure: */
+    pt.x++;
+    pt.y = pt.x + 1;
 
     printf("pt (%p):\n", (void *)&pt);
     printf(" |- x (%p): %d\n", (void *)&pt.x, pt.x);
---
+++ 06.02_structures/point.h
@@ -0,0 +1,5 @@
+/* To define a structure containing two integers: */
+struct point {
+    int x;
+    int y;
+};
