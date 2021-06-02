43a6e96e1bde26eef5f3a88ab12cf6c98a297feb
Christopher Siu <christopher@siu.sh>
2021-06-02 11:00:41 -0700

    Added complete debugging examples.

--- 06.02_structures/memleak.c
+++ 06.02_structures/memleak.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 
 int main(void) {
+    /* NOTE: This is a 16-byte memory leak. */
     int *arr = (int *)malloc(sizeof(int) * 4);
 
     return 0;
--- 06.02_structures/segfault.c
+++ 06.02_structures/segfault.c
@@ -3,5 +3,6 @@
 int main(void) {
     int *ptr = NULL;
 
+    /* NOTE: This will almost certainly result in a segfault. */
     return *ptr;
 }
