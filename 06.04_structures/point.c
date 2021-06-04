1fff5dbf59177ef2cb415ee32fa2c54086dd488f
Christopher Siu <christopher@siu.sh>
2021-06-04 11:21:40 -0700

    Added typedef example.

---
+++ 06.04_structures/point.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "point.h"
+
+int main(void) {
+    /* To declare and initialize a variable of type "struct point": */
+    Point pt = {0, 0};
+
+    /* To mutate the members of a structure: */
+    pt.x++;
+    pt.y = pt.x + 1;
+
+    printf("pt (%p):\n", (void *)&pt);
+    printf(" |- x (%p): %d\n", (void *)&pt.x, pt.x);
+    printf(" +- y (%p): %d\n", (void *)&pt.y, pt.y);
+
+    return 0;
+}
---
+++ 06.04_structures/point.h
@@ -0,0 +1,8 @@
+/* To define "Point" as equivalent to "struct Point": */
+typedef struct point Point;
+
+/* To define a structure containing two integers: */
+struct point {
+    int x;
+    int y;
+};
